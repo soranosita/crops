05841cf971fd92e38917028708ca8496a87f8875
soranosita <113065340+soranosita@users.noreply.github.com>
2022-09-22 17:46:45 +0200

    Rework exception handling
    We are now accounting for request timeouts, connection errors, json decoding errors, and more. When they occur, we retry every n seconds, with n increasing exponentially until we reach the wait time cap or we hit the maximum number of retries. In addition, we display a user-friendly error message instead of the full traceback.

--- src/api.py
+++ src/api.py
@@ -1,9 +1,10 @@
-import time
+from math import exp
+from time import time, sleep
 import json
 
 import requests
 
-from errors import AuthenticationError
+from errors import handle_error, AuthenticationError
 
 
 class GazelleAPI:
@@ -11,45 +12,80 @@ class GazelleAPI:
         self._s = requests.session()
         self._s.headers.update(auth_header)
         self._rate_limit = rate_limit
+        self._timeout = 15
         self._last_used = 0
+
+        self._max_retries = 20
+        self._max_retry_time = 600
+        self._retry_wait_time = lambda x: min(int(exp(x)), self._max_retry_time)
 
         self.sitename = self.__class__.__name__
         self.site_url = site_url
         self.tracker_url = tracker_url
-        self._api_url = f"{self.site_url}/ajax.php"
+        self.api_url = f"{self.site_url}/ajax.php"
         self.announce_url = self._get_announce_url(tracker_url)
 
     def _get(self, action, **params):
-        while True:
-            now = time.time()
+        current_retries = 1
+
+        while current_retries <= self._max_retries:
+            now = time()
             if (now - self._last_used) > self._rate_limit:
                 self._last_used = now
                 params["action"] = action
-                r = self._s.get(self._api_url, params=params)
-                if action == "download":
-                    return r
-                return r.text
+
+                try:
+                    r = self._s.get(self.api_url, params=params, timeout=self._timeout)
+                    if action == "download":
+                        return r
+                    return json.loads(r.text)
+                except requests.exceptions.Timeout as e:
+                    err = "Request timed out", e
+                except requests.exceptions.ConnectionError as e:
+                    err = "Unable to connect", e
+                except requests.exceptions.RequestException as e:
+                    err = "Request failed", f"{type(e).__name__}: {e}"
+                except json.JSONDecodeError as e:
+                    err = "JSON decoding of response failed", e
+
+                handle_error(
+                    description=err[0],
+                    exception_details=err[1],
+                    wait_time=self._retry_wait_time(current_retries),
+                    extra_description=f" (attempt {current_retries}/{self._max_retries})"
+                )
+                current_retries += 1
             else:
-                time.sleep(0.2)
+                sleep(0.2)
+        handle_error(
+            description="Maximum number of retries reached",
+            exit_=True
+        )
 
     def _get_announce_url(self, tracker_url):
-        account_info = self.get_account_info()
-        if account_info["status"] != "success":
-            raise AuthenticationError(f"Invalid API key for {self.sitename}.")
+        try:
+            account_info = self.get_account_info()
+        except AuthenticationError as e:
+            handle_error(
+                description=f"Authentication to {self.sitename} failed",
+                exception_details=e,
+                exit_=True,
+            )
+
         passkey = account_info["response"]["passkey"]
         return f"{tracker_url}/{passkey}/announce"
 
     def get_account_info(self):
-        t = self._get("index")
-        return json.loads(t)
+        r = self._get("index")
+        if r["status"] != "success":
+            raise AuthenticationError(r["error"])
+        return r
 
     def find_torrent(self, hash_):
-        t = self._get("torrent", hash=hash_)
-        return json.loads(t)
+        return self._get("torrent", hash=hash_)
 
     def download_torrent(self, torrent_id):
-        t = self._get("download", id=torrent_id)
-        return t
+        return self._get("download", id=torrent_id)
 
 
 class OPS(GazelleAPI):
@@ -58,7 +94,7 @@ class OPS(GazelleAPI):
             site_url="https://orpheus.network",
             tracker_url="https://home.opsfet.ch",
             auth_header={"Authorization": f"token {api_key}"},
-            rate_limit=delay_in_seconds
+            rate_limit=delay_in_seconds,
         )
 
 
@@ -68,5 +104,5 @@ class RED(GazelleAPI):
             site_url="https://redacted.ch",
             tracker_url="https://flacsfor.me",
             auth_header={"Authorization": api_key},
-            rate_limit=delay_in_seconds
+            rate_limit=delay_in_seconds,
         )
--- src/errors.py
+++ src/errors.py
@@ -1,2 +1,33 @@
+import sys
+from time import sleep
+
+from colorama import Fore
+
+
+def handle_error(
+    description,
+    exception_details=None,
+    wait_time=0,
+    extra_description="",
+    exit_=False,
+):
+    action = "Exiting" if exit_ else "Retrying"
+    action += f" in {wait_time} seconds..." if wait_time else "..."
+    exception_message = (
+        f"\n{Fore.LIGHTBLACK_EX}{exception_details}"
+        if exception_details is not None
+        else ""
+    )
+
+    print(
+        f"{Fore.RED}Error: {description}{extra_description}. {action}"
+        f"{exception_message}{Fore.RESET}"
+    )
+    sleep(wait_time)
+
+    if exit_:
+        sys.exit(1)
+
+
 class AuthenticationError(Exception):
     pass
